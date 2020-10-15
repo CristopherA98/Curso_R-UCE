0f2d4e54211890d0fc42d62b5d0d8ca1f36c9094
Cristopher Aguirre Criollo <cristopher_aguirre98@outlook.com>
2020-10-15 14:59:07 -0500

    Clase 2

--- Unidad 1/Clases/C1-2/C1-2 Estructuras de Control.R
+++ Unidad 1/Clases/C1-2/C1-2 Estructuras de Control.R
@@ -4,19 +4,27 @@
 ####################################
 
 
-# Estructuras de control --------------------------------------------------
 
-#----- Operadores condicional if if else ifelse
+# 1. Estructuras de control -----------------------------------------------
 
-### if
 
+## Información adicional
+browseURL(url = "https://www.datacamp.com/community/tutorials/tutorial-on-loops-in-r",
+          browser = getOption("browser")) # Datacamp
+browseURL(url = "https://bookdown.org/jboscomendoza/r-principiantes4/if-else.html",
+          browser = getOption("browser")) #bookdown
+#----- 1.1 Operadores condicional if if else ifelse
+
+### 1.1.1. if
+
+# Ejemplo 1
 a <-  5
 b <-  5
 if (a == b){
   print("a es igual a b")
 }
 
-### ifelse
+### 1.1.2. ifelse
 
 a <- 7
 b <- 5
@@ -27,7 +35,7 @@ if (a == b){
 }
 
 # OJO 
-# La evaluacion l Â´ ogica no es vectorizada.
+# La evaluacion lógica no es vectorizada.
 x <- c(2, 3, -5, 6, -2, 8)
 if (x > 0) {
   print("positivo")
@@ -35,49 +43,86 @@ if (x > 0) {
   print("negativo")
 }
 
-### ifelse
+### 1.1.3. ifelse
 x <- c(2, 3, -5, 6, -2, 8)
 ifelse(x > 0,"positivo","negativo")
 
-#----- EjecuciÃ³n repetitiva for repeat while
+#----- 1.2. Ejecucion repetitiva for, repeat, while
 
-### for
+### 1.2.1. for
 
 # Ejemplo 1
+
 for (i in 1:4) {
   print(i)
 }
+
 # Ejemplo 2
 x <- c(12, 22, 28, 41)
 for (i in x){
   print(i+5)
 }
 
-### repeat
+### 1.2.2. repeat
 i <- 1
 repeat {
   if (i > 5) break
   else{
-    cat('\n', i, "aÃ±os de edad")
+    cat('\n', i, "años de edad")
     i <- i + 1
   }
 }
 
-### while
+# Funcion cat
+# Esta función escribe texto y variables en la salida.
+# La secuencia de escape "\n" produce una nueva línea e impide que la siguiente 
+# salida del programa quede en la misma línea.
+
+### 1.2.3. while
 
 # Ejemplo 1
+
+"Imprimamos solo los numeros menores a 5"
 i <- 1
 while (i<5) {
   i <- i+1
   print(i)
 }
 
-# Ejemplo 2
+# 2. Familia apply --------------------------------------------------------
 
-i <- 1
-while (i<4) {
-  print(i)
-  i <- i+1
+# Fuente de información 
+browseURL(url = "https://www.guru99.com/r-apply-sapply-tapply.html", browser = getOption("browser")) 
+browseURL(url = "http://adv-r.had.co.nz/Functionals.html", browser = getOption("browser")) 
+browseURL(url = "https://www.datacamp.com/community/tutorials/r-tutorial-apply-family", browser = getOption("browser"))
+
+## 2.1. apply
+data(iris)
+apply(X = iris[,-5],MARGIN = 2,FUN = mean)
+apply(X = iris[,-5],MARGIN = 2,FUN = sum)
+
+## 2.2. lapply
+
+lapply(iris[,-5],FUN = mean)
+lapply(iris, function(x) summary(x))
+
+#sobre un vector
+lapply(1:10, function(x) x^2)
+lapply()
+
+## 2.2. sapply
+
+min.sl <- sapply(iris[,-5],min)
+min.sl #devuelve un vector
+
+min.sl <-lapply(iris[,-5],min)
+min.sl #devuleve una lista
+
+avg <- function (x) {
+  (min(x) + max(x)) / 2
 }
+resultado <- sapply (iris[,-5], avg)
+resultado
 
-
+## 2.2. tapply
+tapply (iris$Sepal.Width, iris$Species, median)
