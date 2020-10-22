7e0f10a4972304c5e1e61630b1ab6a311213fdfc
Cristopher Aguirre Criollo <cristopher_aguirre98@outlook.com>
2020-10-21 23:21:59 -0500

    Actualización

---
+++ Unidad 1/Clases/C1-4/C1-4 Aplicación.R
@@ -0,0 +1,16 @@
+# 1. Ejecutar las siguientes lineas de código
+
+install.packages("devtools")
+devtools::install_github("RamiKrispin/coronavirus")
+
+# 2. Esperar hasta que salga el mensaje siguiente:
+
+#----Enter one or more numbers, or an empty line to skip updates: 
+
+# 3. Escribir "3" en la cosoloa y dar enter
+# 4. cargar el data frame con la siguiente linea de código
+update_dataset()
+covid19 <- data.frame(coronavirus)
+
+
+
