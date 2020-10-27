e2a60f93af5fbf6cc78ca826360350e8ba02f6cc
Cristopher Aguirre Criollo <cristopher_aguirre98@outlook.com>
2020-10-26 19:17:03 -0500

    Actualización

---
+++ Unidad 1/Clases/C1-6/C1-6 Modelo Lineal.R
@@ -0,0 +1,119 @@
+####################################
+###       Curso R - UCE          ###
+###          Clase 06            ###
+####################################
+
+
+# Valores faltantes y atípicos (missing values & outliers) ----------------
+
+packages <- c('tidyverse','ggplot2','visdat','caret','recipes',
+              'mice','Hmisc','DMwR')
+
+install.packages(packages)
+
+library(easypackages)
+libraries(packages)
+
+# 1. Missing values -------------------------------------------------------
+
+## 1.1. Detección
+
+x <- c(1,2,5,8,9,NA)
+is.na(x) # devuleve una respuesta de tipo buleana donde TRUE muestra la presencia de NA's
+which(is.na(x)) # posición de los valores perdidos
+
+data(algae)
+glimpse(algae)
+sum(is.na(algae))
+
+algae %>%
+  is.na() %>%
+  reshape2::melt() %>%
+  ggplot(aes(Var2, Var1, fill=value)) + 
+  geom_raster() + 
+  coord_flip() +
+  scale_y_continuous(NULL, expand = c(0, 0)) +
+  scale_fill_grey(name = "", 
+                  labels = c("Present", 
+                             "Missing")) +
+  xlab("Observación") +
+  theme(axis.text.y  = element_text(size = 10))
+
+### 1.1.1. Paquete Hmisc
+
+sapply(algae,function(x){sum(is.na(x))}) # número de NA´s en la columnas
+round((apply(apply(X = algae,MARGIN = 2,FUN = is.na )
+             ,2,sum)/nrow(algae))*100,2)
+# Eliminar las filas
+data.cleaning <- algae[!is.na(algae$Chla),]
+complete.cases(algae)
+data.cleaning2 <- algae[complete.cases(algae),]
+
+
+## Imputar por la media
+
+algae$Chla_mean <- impute(algae$Chla, mean)
+
+## Imputar con valor aleatorios
+
+algae$Chla_random <- impute(algae$Chla,'random')
+
+algae$Chla_median <- impute(algae$Chla,'median')
+
+### 1.1.2. Paquete mice
+
+imputed_Data <- mice(algae, m=5, maxit = 50, method = 'pmm', seed = 500)
+summary(imputed_Data)
+
+
+algae_mice <- complete(imputed_Data,2)
+sapply(algae_mice,function(x){sum(is.na(x))})
+
+### 1.1.1. Paquete DWwR
+algae_knn<-knnImputation(algae)
+sapply(algae_mice,function(x){sum(is.na(x))})
+
+
+# 2. Outliers -------------------------------------------------------------
+
+data (Ozone, package="mlbench")
+glimpse(Ozone)
+
+# Método IQR
+
+outliers<-function(x){
+  IQR <- quantile(x, probs=c(.25, .75), na.rm = T)
+  ext <- quantile(x, probs=c(.05, .95), na.rm = T)
+  H <- 1.5 * IQR(x, na.rm = T)
+  x[x < (IQR[1] - H)] <- ext[1]
+  x[x > (IQR[2] + H)] <- ext[2]
+  return(x)}
+# Solo variables numeric
+numeric_cols <-names(Ozone)[sapply(Ozone, is.numeric)]
+numeric_data<-Ozone[,names(Ozone)%in%numeric_cols]
+
+Ozone_IQR<- data.frame(sapply(numeric_data,outliers))
+
+ggplot(numeric_data)
+
+# Vecino más cercano
+
+outliers<-function(x){
+  IQR <- quantile(x, probs=c(.25, .75), na.rm = T)
+  H <- 1.5 * IQR(x, na.rm = T)
+  x[x < (IQR[1] - H)] <- NA
+  x[x > (IQR[2] + H)] <- NA
+  return(x)}
+
+numeric_cols<-names(Ozone)[sapply(Ozone, is.numeric)]
+numeric_data<-Ozone[,names(Ozone)%in% numeric_cols]
+
+outliers_missing <-data.frame(sapply(numeric_data,outliers))
+
+Ozone_knn<-knnImputation(outliers_missing)
+
+# FUENTES ADICIONALES -----------------------------------------------------
+
+browseURL(url = "http://openaccess.uoc.edu/webapps/o2/bitstream/10609/64085/6/romancalafatiTFG0617memoria.pdf",
+          browser = getOption("browser"))
+
